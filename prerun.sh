0e22c23546fd9da782859980e527fe77de294082
vladdd183 <vladdd183@gmail.com>
2022-05-30 16:50:35 +0300

    Update prerun.sh

--- prerun.sh
+++ prerun.sh
@@ -2,7 +2,7 @@ if [[ $XXH_SHELL != "xonsh" ]]; then
   CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
   export PYTHONHOME=`dirname $XDG_CONFIG_HOME`/.local
   export PYTHONUSERBASE=$PYTHONHOME
-  export PYTHONPATH=$PYTHONHOME/lib/python3.8/site-packages:$PYTHONPATH
+  export PYTHONPATH=$PYTHONHOME/lib/python3.10/site-packages:$PYTHONPATH
   export PATH=$PYTHONHOME/bin:$CDIR/python:$PATH
 else
     if [[ $XXH_VERBOSE == '1' || $XXH_VERBOSE == '2' ]]; then
